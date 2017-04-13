3a898440a8d6f60a569f4763ebb7d1269cfafce1
Robert Stiff <robert.stiff@just-eat.com>
2017-04-13 15:10:32 +0100

    Separate tests for clarity in the future

---
+++ test/DatabaseTools.Tests/ModifiedTable_Columns.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using DatabaseTools.Model;
+using Xunit;
+
+namespace DatabaseTools.Tests
+{
+    public class ModifiedTable_Columns
+    {
+        [Fact]
+        public void AddedColumns()
+        {
+            var old = new Table {
+                Name = "a new table",
+            };
+
+            var @new = new Table {
+                Name = "a new table",
+                Fields = new [] {
+                    new Field {
+                        Name = "UserId",
+                        Type = "int"
+                    }
+                }
+            };
+
+            var diff = new DiffGenerator().Diff(old.InDbModel(), @new.InDbModel());
+
+            Assert.Equal(1, diff.ModifiedTables.Count);
+            Assert.Equal(true, diff.ModifiedTables.Single().IsModified);
+            Assert.Equal(1, diff.ModifiedTables.Single().AddedColumns.Count());
+            Assert.Equal("UserId", diff.ModifiedTables.Single().AddedColumns.Single().Name);
+            Assert.Equal("int", diff.ModifiedTables.Single().AddedColumns.Single().Type);
+        }
+
+        [Fact]
+        public void RemovedColumns()
+        {
+            var old = new Table {
+                Name = "a new table",
+                Fields = new [] {
+                    new Field {
+                        Name = "UserId",
+                        Type = "int"
+                    }
+                }
+            };
+
+            var @new = new Table {
+                Name = "a new table",
+            };
+
+            var diff = new DiffGenerator().Diff(old.InDbModel(), @new.InDbModel());
+
+            Assert.Equal(1, diff.ModifiedTables.Count);
+            Assert.Equal(true, diff.ModifiedTables.Single().IsModified);
+            Assert.Equal(1, diff.ModifiedTables.Single().RemovedColumns.Count());
+            Assert.Equal("UserId", diff.ModifiedTables.Single().RemovedColumns.Single().Name);
+            Assert.Equal("int", diff.ModifiedTables.Single().RemovedColumns.Single().Type);
+        }
+    }
+}
