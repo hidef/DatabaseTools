fb6d84d4130b4dd7b255b16854b04a3b0c178979
Robert Stiff <robert.stiff@just-eat.com>
2017-04-13 13:53:03 +0100

    Classes in their own files.

--- src/DatabaseTools/Model/TableModification.cs
+++ src/DatabaseTools/Model/TableModification.cs
@@ -2,27 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DatabaseTools.Model   
+namespace DatabaseTools.Model
 {
-    public static class extensions
-    {
-        public static bool EqualTo<T>(this IList<T> self, IList<T> other) where T : class
-        {
-            if ( self == other ) return true;
-            if ( self == null ) return false;
-            if ( other == null ) return false;
-            
-            if ( self.Count() != other.Count() ) return false;
-
-            int count = self.Count();
-            for ( int i = 0; i < count; i++ )
-            {
-                if ( self[i] != other[i] ) return false;
-            }
-
-            return true;
-        }
-    }
     public class TableModification
     {
         private Table @in;
---
+++ src/DatabaseTools/Model/extensions.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseTools.Model
+{
+    public static class extensions
+    {
+        public static bool EqualTo<T>(this IList<T> self, IList<T> other) where T : class
+        {
+            if ( self == other ) return true;
+            if ( self == null ) return false;
+            if ( other == null ) return false;
+            
+            if ( self.Count() != other.Count() ) return false;
+
+            int count = self.Count();
+            for ( int i = 0; i < count; i++ )
+            {
+                if ( self[i] != other[i] ) return false;
+            }
+
+            return true;
+        }
+    }
+}
